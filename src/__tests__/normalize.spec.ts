1a07dc803238190f4e22223ecbf8407f337beca5
Jakub Rożek <jakub@stoplight.io>
2019-06-21 16:08:46 +0800

    feat: improve normalize

--- src/__tests__/normalize.spec.ts
+++ src/__tests__/normalize.spec.ts
@@ -8,4 +8,10 @@ describe('normalize', () => {
   it('ignores POSIX slashes', () => {
     expect(normalize('/d/foo')).toEqual('/d/foo');
   });
+
+  it('does some basic resolving', () => {
+    expect(normalize('/foo/bar/boom/../../baz/.././a')).toEqual('/foo/a');
+    expect(normalize('/foo/bar/boom/../a')).toEqual('/foo/bar/a');
+    expect(normalize('/foo/bar/boom/..')).toEqual('/foo/bar');
+  });
 });
--- src/normalize.ts
+++ src/normalize.ts
@@ -1,6 +1,10 @@
 // https://github.com/isomorphic-git/isomorphic-git/blob/master/src/utils/normalizePath.js
+
+const PARENT_REGEXP = /[^/]+\/\.\.(?:\/|$)/g;
+
 export function normalize(filepath: string) {
-  return filepath
+  PARENT_REGEXP.lastIndex = 0;
+  let normalizedPath = filepath
     .replace(/\\/g, '/') // Replace '\' with '/' (this differs from isomorphic-git)
     .replace(/\/\.\//g, '/') // Replace '/./' with '/'
     .replace(/\/{2,}/g, '/') // Replace consecutive '/'
@@ -8,6 +12,14 @@ export function normalize(filepath: string) {
     .replace(/^\.\/$/, '.') // if path === './' return '.'
     .replace(/^\.\//, '') // Remove leading './'
     .replace(/\/\.$/, '') // Remove trailing '/.'
-    .replace(/(.+)\/$/, '$1') // Remove trailing '/'
-    .replace(/^$/, '.') // if path === '' return '.'
+    .replace(/^$/, '.'); // if path === '' return '.'
+  let segment;
+
+  // tslint:disable-next-line:no-conditional-assignment
+  while ((segment = PARENT_REGEXP.exec(normalizedPath)) !== null) {
+    normalizedPath = `${normalizedPath.slice(0, segment.index)}${normalizedPath.slice(PARENT_REGEXP.lastIndex)}`;
+    PARENT_REGEXP.lastIndex = segment.index - 2;
+  }
+
+  return normalizedPath.replace(/(.+)\/$/, '$1'); // Remove trailing '/'
 }

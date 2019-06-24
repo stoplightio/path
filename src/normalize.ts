867604662ce039e310837dbbf8e31aa4aa7e0aca
William Hilton <wmhilton@gmail.com>
2019-06-24 12:52:02 -0400

    chore: simplify normalize

--- src/normalize.ts
+++ src/normalize.ts
@@ -1,32 +1,16 @@
 // https://github.com/isomorphic-git/isomorphic-git/blob/master/src/utils/normalizePath.js
 
+import * as path from 'path-browserify';
 import * as URI from 'urijs';
 import { isURL } from './isURL';
-
-const PARENT_REGEXP = /[^/]+\/\.\.(?:\/|$)/g;
 
 export function normalize(uri: string) {
   if (isURL(uri)) {
     return new URI(uri).normalize().href();
   }
 
-  PARENT_REGEXP.lastIndex = 0;
-  let normalizedPath = uri
-    .replace(/\\/g, '/') // Replace '\' with '/' (this differs from isomorphic-git)
-    .replace(/\/\.\//g, '/') // Replace '/./' with '/'
-    .replace(/\/{2,}/g, '/') // Replace consecutive '/'
-    .replace(/^\/\.$/, '/') // if path === '/.' return '/'
-    .replace(/^\.\/$/, '.') // if path === './' return '.'
-    .replace(/^\.\//, '') // Remove leading './'
-    .replace(/\/\.$/, '') // Remove trailing '/.'
-    .replace(/^$/, '.'); // if path === '' return '.'
-  let segment;
-
-  // tslint:disable-next-line:no-conditional-assignment
-  while ((segment = PARENT_REGEXP.exec(normalizedPath)) !== null) {
-    normalizedPath = `${normalizedPath.slice(0, segment.index)}${normalizedPath.slice(PARENT_REGEXP.lastIndex)}`;
-    PARENT_REGEXP.lastIndex = segment.index - 2;
-  }
-
-  return normalizedPath.replace(/(.+)\/$/, '$1'); // Remove trailing '/'
+  return path
+    .normalize(uri)
+    .replace(/\\/g, '/') // Replace '\' with '/'
+    .replace(/(.+)\/$/, '$1'); // Remove trailing '/'
 }

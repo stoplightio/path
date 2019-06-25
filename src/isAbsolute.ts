741589de9281aa8c37424a8019ac6878b870871e
Jakub Rożek <jakub@stoplight.io>
2019-06-25 21:33:17 +0200

    refactor: use isURL util in isAbsolute

--- src/isAbsolute.ts
+++ src/isAbsolute.ts
@@ -1,16 +1,20 @@
 import * as URI from 'urijs';
+import { isURL } from './isURL';
 import { startsWithWindowsDrive } from './startsWithWindowsDrive';
 
-const PATH_SEPARATOR_REGEXP = /[/\\]/;
+const PATH_SEPARATOR_REGEXP = /^[/\\]/;
 
 // based on https://github.com/nodejs/node/blob/9946c597077070941309e5b2986a810e52978a97/lib/path.js#L349
-export function isAbsolute(filepath: string) {
-  if (filepath.length === 0) return false;
+export function isAbsolute(uri: string) {
+  if (uri.length === 0) return false;
 
-  try {
-    return PATH_SEPARATOR_REGEXP.test(filepath[0]) || startsWithWindowsDrive(filepath) || URI(filepath).is('absolute');
-  } catch {
-    // merely to catch any URIJS exception that might (or might not) be thrown
-    return false;
+  if (isURL(uri)) {
+    try {
+      return URI(uri).is('absolute');
+    } catch {
+      return false;
+    }
   }
+
+  return PATH_SEPARATOR_REGEXP.test(uri) || startsWithWindowsDrive(uri);
 }

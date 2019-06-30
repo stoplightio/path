a2796bbbb01be913294d437bfd240a01c40b4f1e
William Hilton <wmhilton@gmail.com>
2019-06-29 22:56:13 -0400

    test: add extname tests

--- src/extname.ts
+++ src/extname.ts
@@ -1,7 +1,8 @@
+import { normalizeParsed } from './normalize';
 import { parse, parseBase } from './parse';
 
 export const extname = (path: string) => {
-  const parsed = parse(path);
+  const parsed = normalizeParsed(parse(path));
   const { ext } = parseBase(parsed.base);
   return ext;
 };
--- src/parse.ts
+++ src/parse.ts
@@ -7,14 +7,18 @@ export function parse(path: string): IPath {
 }
 
 export function parseBase(base: string) {
-  const split = base.lastIndexOf('.');
+  let split = base.lastIndexOf('.');
+  // ignore edge cases
+  if (base === '..') split = -1;
+  if (base === '.') split = -1;
+
   let name = base;
-  let ext = null;
+  let ext = '';
   // we check > 0 instead of > -1 so that filenames starting with dots aren't
   // interpreted as file extensions.
   if (split > 0) {
     name = base.slice(0, split);
-    ext = base.slice(split + 1);
+    ext = base.slice(split);
   }
   return { name, ext };
 }

91825b2afdb6893bc2cd780db63f90e74863aa1f
William Hilton <wmhilton@gmail.com>
2019-07-02 13:19:51 -0400

    chore: oops (#4)

--- src/parse.ts
+++ src/parse.ts
@@ -5,20 +5,3 @@ import { IPath } from './types';
 export function parse(path: string): IPath {
   return grammar.parse(path, {});
 }
-
-export function parseBase(base: string) {
-  let split = base.lastIndexOf('.');
-  // ignore edge cases
-  if (base === '..') split = -1;
-  if (base === '.') split = -1;
-
-  let name = base;
-  let ext = '';
-  // we check > 0 instead of > -1 so that filenames starting with dots aren't
-  // interpreted as file extensions.
-  if (split > 0) {
-    name = base.slice(0, split);
-    ext = base.slice(split);
-  }
-  return { name, ext };
-}

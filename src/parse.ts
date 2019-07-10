9353efdf127636f58835bb8bbe830b9c4b294308
William Hilton <wmhilton@gmail.com>
2019-07-10 16:16:23 -0400

    Update parse.ts

--- src/parse.ts
+++ src/parse.ts
@@ -3,5 +3,5 @@ import * as grammar from './grammar';
 import { IPath } from './types';
 
 export function parse(path: string): IPath {
-  return grammar.parse(path, {});
+  return grammar.parse(String(path), {});
 }

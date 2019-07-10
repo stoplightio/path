8b84e6fcbc1f0b478642c6123cf9a47f98b773d0
Jakub Rożek <jakub@stoplight.io>
2019-07-10 10:48:06 +0200

    fix(basename): return extension

--- src/basename.ts
+++ src/basename.ts
@@ -6,6 +6,6 @@ export const basename = (path: string) => {
   const parsed = normalizeParsed(parse(path));
   const base = parsed.path.pop();
   if (!base) return '';
-  const { name } = parseBase(base);
-  return name;
+  const { name, ext } = parseBase(base);
+  return `${name}${ext}`;
 };

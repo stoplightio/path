e23f2d958a32680adfef3ee4d2becee158ad2feb
Jakub Rożek <jakub@stoplight.io>
2023-02-01 16:00:38 +0100

    fix: swich module to commonjs for increased compatibility

--- jest.config.mjs
+++ jest.config.mjs
@@ -1,14 +1,10 @@
 export default {
-  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
   },
   transform: {
     '^.+\\.[tj]s$': [
       'ts-jest',
-      {
-        useESM: true,
-      },
     ],
   },
   testMatch: ['<rootDir>/src/**/__tests__/*.spec.ts'],
